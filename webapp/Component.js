4a9cd60d810e0e8cbdb286bc509e5fa6f6ac23e2
Rawan Mostafa <rawanmostafa.mn@gmail.com>
2026-04-27 21:49:49 +0000

    Add product table columns to annotations

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("fioridevelopmentns.fioridevelopment.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
